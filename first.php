ee012c4ea5d99d0d3c76e723486227ce6d08698b
nish-ahmd <n.ahamed36@gmail.com>
2021-10-03 19:57:03 +0530

    about us

--- first.php
+++ first.php
@@ -323,38 +323,34 @@ Recyclable, Re - useable, Bio Degradeable, Gas - Safe Pot
 
 </div> -->
 
-<div class="container  px-4" id="abt">
-  
-<div class=" text-center heading-section ftco-animate">
-    <p class=" brushfont">About Us</p>
-  </div>
-  <div class="row gx-5">
-    <div class="col">
-     <div class="p-3  ">  <img src="assets/images/longb.jpg" alt=""/></div>
-    </div>
-    <div class="col">
-      <div class="p-3 ">    <h1>Bobos Biriyani</h1>
-    <h2>
-                Believe That Doing Good Is
-            Just As Important As Serving
-            Good Food "Dum Cook" Fresh
-            Biryani For Every Individual
-            Order, And Deliver Biryani In
-            That very Handi To The
-            Customer With Care!
+<div id="about" class="about-main pad-top-100 pad-bottom-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">
+                        <h2 class="brushfont"> About Us </h2>
+                        <h3>IT STARTED, QUITE SIMPLY, LIKE THIS...</h3>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusm incididunt ut labore et dolore magna aliqua. Ut enim ad minim venia, nostrud exercitation ullamco. </p>
 
-</h2>
-    <p>
-                If You Are Among Those Who Find
-            Pleasure In Home Cooked Food, You Are In
-            The Right Place. Preparation OfAll
-            Cuisines With Different Condiments And
-            Finest Ingredients With A Twist Of Sri Lankan Spices Will Give Your Tastebuds A Meal To Remember!
+                        <p> Aenean commodo ligula eget dolor aenean massa. Cum sociis nat penatibu set magnis dis parturient montes, nascetur ridiculus mus. quam felisorat, ultricies nec, Aenean commodo ligula eget dolor penatibu set magnis is parturient montes, nascetur ridiculus mus. quam felisorat, ultricies nec, pellentesque eu, pretium quis, sem. quat massa quis enim. Donec vitae sapien ut libero venenatis fauci Nullam quis ante. Etiam sit amet rci eget eros. </p>
 
-    </p></div>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusm incididunt ut labore et dolore magna aliqua. Ut enim ad minim venia, nostrud exercitation ullamco. </p>
+                    </div>
+                </div>
+                <!-- end col -->
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 justify-content-center">
+                    <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">
+                        <div class="about-images">
+                            <img class="about-main" src="assets/images/biriyani_red.jpeg" alt="About Main Image">
+                        </div>
+                    </div>
+                </div>
+                <!-- end col -->
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
     </div>
-  </div>
-</div>
 <!------------------------------------------------about us ends here ------------------------------------------------------------------>
 
 <!------------------------------------------------CUSTOMER REVIEW ------------------------------------------------------------------>
